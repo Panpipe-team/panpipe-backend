04988285cded994703cb16ea3359b67a155b3848
gri-gri <46087070+gri-gri@users.noreply.github.com>
2024-12-08 17:05:14 +0400

    feat: add tags to HabitParamsSet

--- src/Panpipe/Domain/HabitParamsSet/HabitParamsSet.cs
+++ src/Panpipe/Domain/HabitParamsSet/HabitParamsSet.cs
@@ -1,19 +1,29 @@
 using Panpipe.Domain.HabitResult;
+using Panpipe.Domain.Tags;
 
 namespace Panpipe.Domain.HabitParamsSet;
 
 public class HabitParamsSet
 {
+    private readonly List<Tag> _tags;
+
     #pragma warning disable CS8618 // Required by Entity Framework
     private HabitParamsSet() {}
 
     public HabitParamsSet(
-        Guid id, string name, string description, AbstractHabitResult goal, Frequency frequency, bool isPublicTemplate
+        Guid id, 
+        string name, 
+        string description, 
+        List<Tag> tags, 
+        AbstractHabitResult goal, 
+        Frequency frequency, 
+        bool isPublicTemplate
     )
     {
         Id = id;
         Name = name;
         Description = description;
+        _tags = tags;
         Goal = goal;
         Frequency = frequency;
         IsPublicTemplate = isPublicTemplate;
@@ -26,6 +36,7 @@ public class HabitParamsSet
     public Frequency Frequency { get; init; }
     public bool IsPublicTemplate { get; init; }
 
+    public ICollection<Tag> Tags => _tags.AsReadOnly();
     public HabitResultType ResultType => Goal.Type;
 
     public List<DateTimeOffset> CalculateTimestampsOfEmptyMarksForNewlyCreatedHabit() 
